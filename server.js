e2ed67128f3b9f10bf5b66e5a236965720cce689
Federico Macías <fma.abogado@gmail.com>
2020-10-29 11:02:42 -0600

    Mi primer commit

---
+++ server.js
@@ -0,0 +1,31 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const indexRouter = require("./routes/index");
+
+//Settings
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((db) => console.log("Database is connected to mongoose"))
+  .catch((err) => console.log(err));
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running on port 5000");
+});
