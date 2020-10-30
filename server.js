c053ce97aa78aa3d43e8dd6ad69445ceb4334e3c
Federico Macías <fma.abogado@gmail.com>
2020-10-29 21:21:27 -0600

    Se añade las rutas para los autores

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-
+//Imports
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 //Settings
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
+//bodyparser
+const bodyParser = require("body-parser");
+
+//Static
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+
+//DB connection
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -25,6 +33,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server running on port 5000");
